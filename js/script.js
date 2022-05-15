5e1d669c999cb91a4f3a86a2e40bdbec3dff364a
Michele Bisceglia <mikael_bisceglia@libero.it>
2022-05-15 13:36:29 +0200

    email generation

--- js/script.js
+++ js/script.js
@@ -1,4 +1,22 @@
-axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-.then( function(response) {
-    console.log(response);
-})
+app = new Vue(
+    {
+        el:'#root',
+        data: {
+            list: [],
+        },
+        created() {
+                for (let i = 0; i < 10; i++) {
+                axios
+                .get('https://flynn.boolean.careers/exercises/api/random/mail') 
+                .then((resp) => {
+                const email = resp.data.response;
+                console.log(email);
+                this.list.push(email)
+                })}
+            }
+        }
+        
+    
+);
+
+ 
