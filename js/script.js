027d9e1e9ea6deda30fcab242f16f32c71ed1d49
Michele Bisceglia <mikael_bisceglia@libero.it>
2022-05-15 23:36:49 +0200

    css

--- js/script.js
+++ js/script.js
@@ -4,16 +4,19 @@ app = new Vue(
         data: {
             list: [],
         },
-        created() {
-                for (let i = 0; i < 10; i++) {
-                axios
-                .get('https://flynn.boolean.careers/exercises/api/random/mail') 
-                .then((resp) => {
-                const email = resp.data.response;
-                console.log(email);
-                this.list.push(email)
-                })}
-            },
+        
+            methods: {
+                generateEmail() {
+                    for (let i = 0; i < 10; i++) {
+                        axios
+                        .get('https://flynn.boolean.careers/exercises/api/random/mail') 
+                        .then((resp) => {
+                        const email = resp.data.response;
+                        console.log(email);
+                        this.list.push(email)
+                        })}
+                }
+            }
         }
         
         
